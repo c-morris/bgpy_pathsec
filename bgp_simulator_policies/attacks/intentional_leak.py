f895b457a00050cbec1adfd6c58bc070b68ca92f
Cameron Morris <cameron.morris@uconn.edu>
2021-09-13 01:50:12 +0000

    add intentional leak attack

---
+++ bgp_simulator_policies/attacks/intentional_leak.py
@@ -0,0 +1,35 @@
+from lib_bgp_simulator import BGPPolicy, Attack, Prefixes, Timestamps, ASNs, Announcement, Relationships, Scenario
+
+from .. import DOAnn
+
+class IntentionalLeak(Attack):
+    def __init__(self, attacker=ASNs.ATTACKER.value, victim=ASNs.VICTIM.value):
+        anns = [DOAnn(prefix=Prefixes.PREFIX.value,
+                    timestamp=Timestamps.VICTIM.value,
+                    as_path=(victim,),
+                    seed_asn=victim)]
+        super(IntentionalLeak, self).__init__(attacker, victim, anns)
+        self.round = 0
+        def hook(s: Scenario):
+            # Add the route leak from the attacker
+            attacker_ann = None
+            self.round += 1
+            if self.round == 1:
+                attacker_ann = s.engine.as_dict[attacker].policy.local_rib.get(Prefixes.PREFIX.value)
+                # If the attacker never received, the announcement, this attack is impossible, return
+                if attacker_ann is None: 
+                    print("Attacker did not receive announcement from victim, cannot attack")
+                    print("Attacker RIB WAS", s.engine.as_dict[attacker].policy.local_rib)
+                    return
+                print("Seeding attack announcement:", attacker_ann)
+                atk_ann = DOAnn(prefix=Prefixes.PREFIX.value,
+                    timestamp=Timestamps.ATTACKER.value,
+                    as_path=attacker_ann.as_path,
+                    seed_asn=attacker)
+                atk_ann.recv_relationship = Relationships.CUSTOMERS
+                # Clear any down only communities
+                atk_ann.do_communities = tuple()
+                self.announcements.append(atk_ann)
+        self.post_run_hooks = [hook]
+
+
