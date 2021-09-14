1d8ebc1db9d2eb0db90a7caf7e1aac72cd1e541f
Cameron Morris <cameron.morris@uconn.edu>
2021-09-14 00:17:56 +0000

    fix attacks to modify the announcement in-place

--- bgp_simulator_policies/attacks/accidental_leak.py
+++ bgp_simulator_policies/attacks/accidental_leak.py
@@ -9,25 +9,24 @@ class AccidentalLeak(Attack):
                     as_path=(victim,),
                     seed_asn=victim)]
         super(AccidentalLeak, self).__init__(attacker, victim, anns)
+        
         self.round = 0
-        def hook(s: Scenario):
-            # Add the route leak from the attacker
-            attacker_ann = None
-            self.round += 1
-            if self.round == 1:
-                attacker_ann = s.engine.as_dict[attacker].policy.local_rib.get(Prefixes.PREFIX.value)
-                # If the attacker never received, the announcement, this attack is impossible, return
-                if attacker_ann is None: 
-                    print("Attacker did not receive announcement from victim, cannot attack")
-                    print("Attacker RIB WAS", s.engine.as_dict[attacker].policy.local_rib)
-                    return
-                print("Seeding attack announcement:", attacker_ann)
-                atk_ann = DOAnn(prefix=Prefixes.PREFIX.value,
-                    timestamp=Timestamps.ATTACKER.value,
-                    as_path=attacker_ann.as_path,
-                    seed_asn=attacker)
-                atk_ann.recv_relationship = Relationships.CUSTOMERS
-                self.announcements.append(atk_ann)
-        self.post_run_hooks = [hook]
+        self.post_run_hooks = [self.hook]
+
+    def hook(self, s: Scenario):
+        # Add the route leak from the attacker
+        attacker_ann = None
+        self.round += 1
+        if self.round == 1:
+            attacker_ann = s.engine.as_dict[self.attacker_asn].policy.local_rib.get(Prefixes.PREFIX.value)
+            # If the attacker never received, the announcement, this attack is impossible, return
+            if attacker_ann is None: 
+                print("Attacker did not receive announcement from victim, cannot attack")
+                print("Attacker RIB WAS", s.engine.as_dict[self.attacker_asn].policy.local_rib)
+                return
+            print("Altering the recv_relationship to customer for:", attacker_ann)
+            s.engine.as_dict[self.attacker_asn].policy.local_rib[Prefixes.PREFIX.value].recv_relationship = Relationships.CUSTOMERS
+        self.announcements.clear()
+
 
 
