49437105d90821d035e8f561da5826371f58c814
Cameron Morris <cameron.morris@uconn.edu>
2021-11-08 01:52:20 +0000

    add test

---
+++ bgp_simulator_policies/tests/yaml_small_tests03/test_small_yaml.py
@@ -0,0 +1,38 @@
+import pytest
+from pathlib import Path
+
+from lib_caida_collector import PeerLink, CustomerProviderLink as CPLink
+from lib_bgp_simulator import Relationships, BGPAS, Relationships, LocalRIB
+from lib_bgp_simulator import ASNs, BaseGraphSystemTester, YamlSystemTestRunner
+
+from bgp_simulator_policies import PTestAnn, DownOnlyAS, BGPsecAS, BGPsecTransitiveAS, BGPsecTransitiveDownOnlyAS, IntentionalLeak
+from ..graphs import PGraph007
+
+
+class Test006BGPsecPreference(BaseGraphSystemTester):
+    GraphInfoCls = PGraph007
+    EngineInputCls = IntentionalLeak
+    base_dir = Path(__file__).parent
+    BaseASCls = BGPAS
+    AdoptASCls = BGPsecAS
+    propagation_rounds = 2
+    adopting_asns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 777]
+
+class Test006BGPsecTransitivePreference(BaseGraphSystemTester):
+    GraphInfoCls = PGraph007
+    EngineInputCls = IntentionalLeak
+    base_dir = Path(__file__).parent
+    BaseASCls = BGPAS
+    AdoptASCls = BGPsecTransitiveAS
+    propagation_rounds = 2
+    adopting_asns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 777]
+
+class Test006BGPsecTransitiveDownOnlyPreference(BaseGraphSystemTester):
+    GraphInfoCls = PGraph007
+    EngineInputCls = IntentionalLeak
+    base_dir = Path(__file__).parent
+    BaseASCls = BGPAS
+    AdoptASCls = BGPsecTransitiveDownOnlyAS
+    propagation_rounds = 2
+    adopting_asns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 777]
+
