e2a71fc2173aa5da790b1a831d436897221c8d53
Cameron Morris <cameron.morris@uconn.edu>
2021-11-26 15:43:38 -0500

    merge conflicts

--- bin/intentional_leak_down_only.py
+++ bin/intentional_leak_down_only.py
@@ -8,7 +8,9 @@ from bgp_simulator_policies import IntentionalLeak, LeakGraph, PAnn, DownOnlyAS,
 from lib_bgp_simulator import Simulator, Graph, ROVAS, SubprefixHijack, BGPAS, MPMethod
 
 graphs = [LeakGraph(
-                percent_adoptions=[1, 10, 20, 50, 80, 99],
+                #percent_adoptions=[1, 10, 20, 50, 80, 99],
+                #percent_adoptions=[80, 99],
+                percent_adoptions=[100],
                 adopt_as_classes=[BGPAS, BGPsecTransitiveAS, BGPsecTransitiveDownOnlyAS], 
                 EngineInputCls=IntentionalLeak,
                 num_trials=3000,
