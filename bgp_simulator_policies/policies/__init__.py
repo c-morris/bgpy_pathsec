aa8c4871cff4d64b5093415e7883bd785b3a5175
Cameron Morris <cameron.morris@uconn.edu>
2022-10-05 00:09:34 +0000

    lints

--- bgp_simulator_policies/policies/__init__.py
+++ bgp_simulator_policies/policies/__init__.py
@@ -3,4 +3,4 @@ from .bgpsec_transitive import BGPsecTransitiveAS # noqa F401
 from .do import DownOnlyAS # noqa F401
 from .bgpsec_transitive_do import BGPsecTransitiveDownOnlyAS # noqa F401
 from .aggregator_variants import * # noqa F401
-from .path_end import PathEndAS
+from .path_end import PathEndAS # noqa F401
