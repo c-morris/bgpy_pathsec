13765f616d3bb1e7b6158d525b716fb54cf9d710
Cameron Morris <cameron.morris@uconn.edu>
2022-12-11 13:49:14 -0700

    maybe multiple inheritance wasn't such a good idea

--- bgp_simulator_pathsec_policies/attacks/shortest_path_export_all_no_hash.py
+++ bgp_simulator_pathsec_policies/attacks/shortest_path_export_all_no_hash.py
@@ -2,8 +2,11 @@ from .intentional_leak_no_hash import IntentionalLeakNoHash
 from .shortest_path_export_all import ShortestPathExportAll
 
 
-class ShortestPathExportAllNoHash(IntentionalLeakNoHash, ShortestPathExportAll):
+class ShortestPathExportAllNoHash(ShortestPathExportAll):
     """Shortest path Export all strategy, from other works.
     Only leaks a single path (the shortest one) to providers.
     Assumes no hash chain to prevent path shortening.
     """
+
+ShortestPathExportAllNoHash._truncate_ann = \
+    IntentionalLeakNoHash._truncate_ann
