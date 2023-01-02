7ed0e37ba0fc2614148832cd0347a87bad25bdb0
Cameron Morris <cameron.morris@uconn.edu>
2023-01-02 20:28:45 +0000

    fix inheritance

--- bgp_simulator_pathsec_policies/policies/transitive_dropping.py
+++ bgp_simulator_pathsec_policies/policies/transitive_dropping.py
@@ -14,6 +14,8 @@ class TransitiveDroppingAS(BGPAS):
         # Set the probability of dropping transitive attrs for *this* AS only
         self.transitive_dropping = (
             random.SystemRandom().random() < (self.transitive_dropping_percent / 100.0))
+        if self.transitive_dropping:
+            print('initialized transitive dropping')
 
         super(TransitiveDroppingAS, self).__init__(*args,
                                                    **kwargs)
@@ -28,12 +30,12 @@ class TransitiveDroppingAS(BGPAS):
             ann_to_send.bgpsec_path = tuple()
         super(TransitiveDroppingAS, self)._process_outgoing_ann(as_obj, ann_to_send, propagate_to, send_rels, *args, **kwargs) # noqa E501
 
-class TransitiveDropping2AS(BGPAS):
+class TransitiveDropping2AS(TransitiveDroppingAS):
     """Drops transitive attributes with some probability"""
     name = "TransitiveDropping2AS"
     transitive_dropping_percent=2.0
 
-class TransitiveDropping4AS(BGPAS):
+class TransitiveDropping4AS(TransitiveDroppingAS):
     """Drops transitive attributes with some probability"""
     name = "TransitiveDropping4AS"
     transitive_dropping_percent=4.0
