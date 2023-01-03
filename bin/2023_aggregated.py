828671c8f4af0ceb9689abcb4559cfc4665842e9
Cameron Morris <cameron.morris@uconn.edu>
2023-01-03 22:42:59 +0000

    add path length subgraph to aggregated script

--- bin/2023_aggregated.py
+++ bin/2023_aggregated.py
@@ -64,6 +64,7 @@ from bgp_simulator_pathsec_policies import OverheadAllSubgraph
 from bgp_simulator_pathsec_policies import OverheadBPOAllSubgraph
 from bgp_simulator_pathsec_policies import AdoptingCountSubgraph
 from bgp_simulator_pathsec_policies import NonAdoptingCountSubgraph
+from bgp_simulator_pathsec_policies import PathLengthSubgraph
 from bgp_simulator_pathsec_policies import ShortestPathExportAll
 from bgp_simulator_pathsec_policies import BGPsecTransitiveDownOnlyNoHashUpTimidAS
 from bgp_simulator_pathsec_policies import PathEndAggressiveAS
@@ -174,6 +175,7 @@ sim = Simulation(
         AttackerSuccessAllSubgraph(),
         VictimSuccessAllSubgraph(),
         DisconnectedAllSubgraph(),
+        PathLengthSubgraph(),
         AttackerSuccessAdoptingEtcSubgraph(),
         AttackerSuccessAdoptingInputCliqueSubgraph(),
         AttackerSuccessAdoptingStubsAndMHSubgraph(),
