b0ad145cbedf1fe9dd198769ecdd2676ea961770
Justin Furuness <jfuruness@gmail.com>
2023-11-11 15:25:04 -0500

    tests passing

--- bgpy_pathsec/attacks/mh_path_manipulation.py
+++ bgpy_pathsec/attacks/mh_path_manipulation.py
@@ -2,7 +2,8 @@ from bgpy import Scenario, Prefixes, Relationships, Timestamps
 
 from ..policies import BGPsecAS, BGPsecTransitiveAS
 from ..policies import TransitiveDroppingNoAdoptCustomersAS
-from ..policies import TransitiveDroppingAlwaysAS
+from ..policies import TransitiveDroppingNoAdoptCustomersAlwaysAS
+from ..policies import TransitiveDroppingNeverAS
 
 
 class MHPathManipulation(Scenario):
@@ -44,7 +45,7 @@ class MHPathManipulation(Scenario):
         BGPsecTransitiveAS.bpo_count = 0
         TransitiveDroppingNoAdoptCustomersAS.convert_count = 0
         super().setup_engine(*args, **kwargs)
-        if self.scenario_config.BaseASCls == TransitiveDroppingAlwaysAS:
+        if self.scenario_config.BaseASCls == TransitiveDroppingNoAdoptCustomersAlwaysAS:
             self.as_classes_used = self.as_classes_used | frozenset(
-                [TransitiveDroppingAlwaysAS]
+                [TransitiveDroppingNeverAS]
             )
